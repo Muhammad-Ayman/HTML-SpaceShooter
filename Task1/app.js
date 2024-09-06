20174001174d77ef07ecaa48575d59e558f23304
Muhammad-Ayman <i.muhammadayman@gmail.com>
2024-09-06 17:14:28 +0300

    Responsive

--- Task1/app.js
+++ Task1/app.js
@@ -8,13 +8,7 @@ let imglist = document.querySelector(".imgList");
 let main = document.querySelector(".mainImg");
 let mainImg = main.querySelector("img");
 let imgElements = [];
-let getProducts = async () => {
-  let response = await fetch("https://fakestoreapi.com/products");
-  let data = await response.json();
-  console.log(data);
-  return data;
-};
-getProducts();
+
 for (img of imgs) {
   let imgElement = document.createElement("img");
   imgElement.src = img;
